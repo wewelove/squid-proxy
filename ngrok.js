15975642a11d0355659798ce712c9d0b0775b5c4
MRakox <rakoxwastaken@gmail.com>
2022-08-04 10:27:21 +0200

    ✨ Introduce ngrok service

---
+++ ngrok.js
@@ -0,0 +1,37 @@
+import ngrok from 'ngrok';
+
+/**
+ * Switch the protocol used in the URL (from TCP to HTTP)
+ * @param {string} url The URL to switch
+ * @returns {string} The URL with the protocol switched
+ */
+function switchProtocol(url) {
+  return url?.replace('tcp://', 'http://');
+}
+
+/**
+ * Connect to a ngrok tunnel.
+ * If a tunnel is already running, it will return the public url of the tunnel.
+ * If not, it will create a new tunnel.
+ * @param {{
+ *  port: number,
+ *  protocol: 'http' | 'tcp',
+ *  service: string
+ * }} options The options to use for the tunnel
+ * @returns {() => Promise<string>} An async function that returns the public url of the tunnel
+ */
+export default ({ port, protocol, service }) => async () => {
+  // Check if a tunnel is already running
+  const response = await ngrok.getApi()?.listTunnels();
+  const tunnel = response?.tunnels?.find((value) => value.proto === protocol);
+  if (tunnel) return switchProtocol(tunnel.public_url);
+  // Create a tunnel to the proxy server
+  const url = switchProtocol(
+    await ngrok.connect({
+      addr: port,
+      proto: protocol,
+    }),
+  );
+  console.log(`[${service.toUpperCase()}] 🚀 Tunnel ready at ${url}`);
+  return url;
+};
